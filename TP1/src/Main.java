8b6ca07205f17313d18ccaed9de7cac1e7990b5f
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-04-14 11:58:39 -0300

    Se agregaron nuevas carpetas

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -1,2 +1,31 @@
+import task.ConsumidoresDeDatos;
+import task.CreadorDeDatos;
+import task.Dato;
+import task.RevisoresDeDatos;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 public class Main {
+    ArrayList<Dato> bufferInicial;
+    ArrayList<Dato> bufferFinal;
+    //ArrayList<CreadorDeDatos> listaDeCreador = new ArrayList<>(4);
+    //ArrayList<RevisoresDeDatos> revisoresDeDatos = new ArrayList<>(2);
+    //ArrayList<ConsumidoresDeDatos> consumidoresDeDatos = new ArrayList<>(2);
+
+    public static void main (String[] args) {
+    }
+
+
+
+    public static void logger(ArrayList<String> charcito){
+        try (FileWriter file = new FileWriter("Data/log.txt"); PrintWriter pw = new PrintWriter(file);) {
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ TP1/src/task/CreadorDeDatos.java
@@ -0,0 +1,12 @@
+package task;
+
+public class CreadorDeDatos implements Runnable{
+
+    public CreadorDeDatos() {
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
