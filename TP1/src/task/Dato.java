47313e6e30c9e365e31774e52fef6741db835c6f
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-04-14 18:22:40 -0300

    Se creo el constructor de la clase, y se declararon métodos para cada una de las clases que están vivas

--- TP1/src/task/Dato.java
+++ TP1/src/task/Dato.java
@@ -1,4 +1,40 @@
 package task;
 
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 public class Dato {
+
+    private int reviews;
+    private Boolean validado;
+    private Semaphore semCrear;
+    private Semaphore semRevisar;
+    private Semaphore semConsumir;
+    private final Object llaveLectura;
+
+    public Dato() {
+        reviews = 0;
+        validado = false;
+        semCrear = new Semaphore(1, true);
+        semRevisar = new Semaphore(2, true);
+        semConsumir = new Semaphore(1, true);
+        llaveLectura = new Object();
+    }
+
+    public void escribir() throws InterruptedException {
+    }
+
+    public void revisar() throws InterruptedException {
+    }
+
+    public void eliminar() throws InterruptedException {
+    }
+
+    public int getReviews() {
+        return reviews;
+    }
+
+    public Boolean getValidado() {
+        return validado;
+    }
 }
