d2c3b86de935b7ec90cd8e5d459bde445770d6d1
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-04-14 18:21:16 -0300

    Se creo el constructor de la clase

--- TP1/src/task/ConsumidoresDeDatos.java
+++ TP1/src/task/ConsumidoresDeDatos.java
@@ -1,4 +1,20 @@
 package task;
 
-public class ConsumidoresDeDatos {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ConsumidoresDeDatos implements Runnable {
+
+    private ArrayList<Dato> bufferInicial;
+    private ArrayList<Dato> bufferDeValidados;
+
+    public ConsumidoresDeDatos(ArrayList<Dato> bufferInicial, ArrayList<Dato> bufferDeValidados) {
+        this.bufferInicial = (ArrayList) bufferInicial.clone();
+        this.bufferDeValidados = (ArrayList) bufferDeValidados.clone();
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
