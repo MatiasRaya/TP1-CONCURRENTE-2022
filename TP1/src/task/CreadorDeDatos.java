2eeede993ee0ba9cb00f8da2051b2d9f5064b0e5
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-04-14 21:15:00 -0300

    Se modifico el método run()

--- TP1/src/task/CreadorDeDatos.java
+++ TP1/src/task/CreadorDeDatos.java
@@ -1,18 +1,17 @@
 package task;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class CreadorDeDatos implements Runnable{
 
     private ArrayList<Dato> bufferInicial;
 
     public CreadorDeDatos(ArrayList<Dato> bufferInicial) {
-        this.bufferInicial = (ArrayList) bufferInicial.clone();
+        this.bufferInicial = (ArrayList<Dato>) bufferInicial.clone();
     }
 
     @Override
     public void run() {
-
+        bufferInicial.add(0, new Dato("E"));
     }
 }
